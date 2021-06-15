f11376a5d76a0975396708312bdec421a43a72ac
“YMES“ <454554317@qq.com>
2021-06-15 15:43:00 +0800

    111

--- src/components/qr.vue
+++ src/components/qr.vue
@@ -1,74 +1,72 @@
 <template>
   <div>
-      <vue-qr :text="appSrc"
-        :margin="0"
-        :size = "191"
-        :logo-src = "logoSrc"      
-      />
+    <vue-qr :text="appSrc" :margin="0" :size="191" :logo-src="logoSrc" />
   </div>
 </template>
 
 <script>
-import VueQr from 'vue-qr'
+import VueQr from "vue-qr";
 export default {
-    name : "vueqr",
-    data(){
-        return{
-            logoSrc:"",
-            appSrc :""
-        }
+  name: "vueqr",
+  data() {
+    return {
+      logoSrc: "",
+      appSrc: "",
+      status : '',
+    };
+  },
+  components: {
+    VueQr,
+  },
+  methods: {
+    async get_login_key() {
+        
+        let res = await this.$api.getQrCode();
+        let code = res.data.unikey;
+        console.log("qrcode", code);
+        return code;
+    
+      
     },
-    components:{
-        VueQr
+
+    async get_login_pic() {
+      try {
+        let res1 = await this.$api.getQrCode();
+        let res = await this.$api.getQrPic(res1);
+        let data = res.data.qrurl;
+        this.appSrc = data;
+        console.log(data);
+        return data;
+      } catch (e) {
+        console.log(e);
+      }
     },
-    methods:{
-        async get_login_key(){
-            try{
-                let res = await this.$api.getQrCode();
-                let code = res.data.unikey
-                console.log(code)
-                return code
-            }catch(e){
-                console.log(e)
-            }
-            
-        },
 
-        async get_login_pic(){
-            let pic_code = this.get_login_key()
-            console.log(pic_code)
-            try{
-                let res = await this.$api.getQrPic(pic_code);               
-                let data = res.data.qrurl
-                this.appSrc = data
-                console.log(data)
-                return data
-            }catch(e){
-                console.log(e)
-            }
-        },
+    //无语了，就是获取不到状态
+    async get_qr_status() {
+      let res = await this.$api.getQrCode();
+      let data = res.data.unikey
+      console.log(res.data.unikey)
+      if (res) {
+          setInterval(() => {
+            console.log(data)
+          let sta = this.$api.getQrSta(data);
+          console.log(sta);
+          let status = sta.message;
+          //console.log(status);
+        }, 10000);
+        // if(sta){
 
-         async get_qr_status(){
-             let pic_code = this.get_login_key()
-             try{
-                 let sta = await this.$api.getQrSta(pic_code);
-                 console.log(sta)
-                 let status = sta.message
-                 console.log(status)
-                 return data
-             }catch(e){
-                 console.log(e)
-             }
-         }
-
+        // }
+      }
     },
-     mounted(){
-        this.get_login_pic()
-        this.get_qr_status()
-    }
-}
+  },
+  mounted() {
+    this.get_login_pic();
+    //this.get_qr_status();
+  },
+};
 </script>
 
 <style>
-
 </style>
